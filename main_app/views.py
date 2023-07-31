fe3a47eaa44a11603781d220194ca3d6df2c52ee
Agathe Lavigne <gotgini@gmail.com>
2023-07-31 15:55:07 +0200

    finch navbar and link functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+finches = [
+  {'name': 'Cactus', 'breed': 'Hawfinch', 'scientific_name': 'Coccothraustes coccothraustes', 'age': 3},
+  {'name': 'Mochi', 'breed': 'Purple Finch', 'scientific_name': 'Haemorhous purpureus', 'age': 0},
+]
+
+def home(request):
+    return render(request, 'home.html')
+	
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+	})
